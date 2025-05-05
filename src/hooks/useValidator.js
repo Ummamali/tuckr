a81af1c817a94998eb6a68b0ec8d4009ce3cc92a
Ummam Ali (Lappy) <sblue7446@gmail.com>
2025-05-05 20:39:34 +0500

    changing use validator hook

--- src/hooks/useValidator.js
+++ src/hooks/useValidator.js
@@ -33,38 +33,26 @@ function validityReducer(state, action) {
     return state;
   }
 }
-export default function useValidator(
-  identityList,
-  validatorPredicates,
-  asyncList = []
-) {
+export default function useValidator(validator) {
   /* 
   Inputs:
-  >>>>> identity list: object ---> {[identity]: [error-message]}
-  >>>>> validatorPredicates: object ----> {[identity]: predicateFunc}
-      Note: the predicateFunc should return an object {*isValid: boolean, msg: String | null}
+  >>>>> validator: object ----> {[identity]: predicateFunc}
+      Note: object of objects having a property "validate" which should return/resolve an object {*isValid: boolean, msg: String | null}
       Though it is not necessary to return a msg, if msg has a falsy value default error msg provided from identityList will be used
-  >>>>> asyncList: [identity (String)] array for identities that are validated asynchronously
-   the validator will check if the predicate lies in async array to use then and catch accordingly. Make sure to make that function async
    */
 
   /*
     Signature:
-      const [validityStatus, dispatchValidity, validate] = useValidator(
-    identityList,
-    validatorPredicates
-  );
+      const {validityStatus, dispatchValidity, validate} = useValidator(validator);
   
   */
 
   // Initial value will be calculated only for the first time
   const initialValue = useMemo(() => {
-    const answer = {};
-    for (const [identity, message] of Object.entries(identityList)) {
-      answer[identity] = { vStatus: 0, msg: message };
-    }
-    return answer;
-  }, []);
+    return Object.fromEntries(
+      Object.keys(validator).map((key) => [key, { vStatus: 0, msg: null }])
+    );
+  }, [validator]);
 
   const [validityStatuses, dispatchValidity] = useReducer(
     validityReducer,
@@ -72,7 +60,7 @@ export default function useValidator(
   );
 
   // this is the majic function which will validate
-  function validate(identity, value) {
+  async function validate(identity, value) {
     /* 
     The validate function returns a value(boolean) if the validator is synchronous
     and will return a promise(will resolve to a boolean) if the vaidator is asynchronous 
@@ -80,58 +68,29 @@ export default function useValidator(
     Dont worry if you are just interested in validation, validation will be done automatically
 
     */
-    const vFunc = validatorPredicates[identity];
-    if (asyncList.includes(identity)) {
-      return new Promise((resolve) => {
-        // async validation
-        dispatchValidity(
-          vActions.SET({ identity: identity, new: { vStatus: 1 } })
-        );
-        vFunc(value)
-          .then((validityResponse) => {
-            const isValid = validityResponse.isValid;
-            const newMessage = validityResponse.msg
-              ? validityResponse.msg
-              : identityList[identity];
-            const newStatus = isValid ? 2 : 3;
-            dispatchValidity(
-              vActions.SET({
-                identity: identity,
-                new: { vStatus: newStatus, msg: newMessage },
-              })
-            );
-            resolve(isValid);
-          })
-          .catch((error) => {
-            dispatchValidity(
-              vActions.SET({
-                identity: identity,
-                new: { vStatus: 3, msg: "Network Error: Failed to validate" },
-              })
-            );
-            // the rejection has been handled above so the returned value is false
-            resolve(false);
-          });
-      });
-    } else {
-      // synchronous validation
-      let vStatus = 2;
-      let msg = null;
-      try {
-        vFunc.validateSync(value);
-      } catch (err) {
-        vStatus = 3;
-        msg = err.message;
-      }
+    // async validation
+    dispatchValidity(vActions.SET({ identity: identity, new: { vStatus: 1 } }));
+
+    try {
+      await validator[identity].validate(value);
+    } catch (err) {
       dispatchValidity(
         vActions.SET({
           identity: identity,
-          new: { vStatus, msg },
+          new: { vStatus: 3, msg: err.message },
         })
       );
-      return vStatus === 2;
+      return 3;
     }
+    dispatchValidity(
+      vActions.SET({
+        identity: identity,
+        new: { vStatus: 2, msg: null },
+      })
+    );
+    return 2;
   }
+
   return { validityStatuses, dispatchValidity, validate };
 }
 
